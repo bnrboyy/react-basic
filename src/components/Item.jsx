d7c6be77cad52f045c41d53702e327454ed13ac7
bnrboyy <nantachai.ru@gmail.com>
2023-03-02 16:36:58 +0700

    ss

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,18 @@
+import Protypes from 'prop-types';
+
 const Item = (props) => {
+  const {title, amount} = props // Props Destructuring
   return (
     <li>
-      {props.title} <span>{props.amount}</span>
+      {title} <span>{amount}</span>
     </li>
   );
-};
+}
+
+/* proptype validator */
+Item.propTypes = {
+  title:Protypes.string.isRequired,
+  amount:Protypes.number.isRequired
+}
 
 export default Item;
--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -6,12 +6,15 @@ const Transaction = () => {
     {title: "ค่ารักษาพยาบาล", amount: 2000},
     {title: "ค่ารถ", amount: 5000},
     {title: "ค่าเช่าบ้าน", amount: 4500},
+    {title: "ค่าไฟ", amount: 500},
+    {title: "ค่าเดินทาง", amount: 800},
   ]
   return (
     <ul className='item-list'>
-      <Item title={data[0].title} amount={data[0].amount}/>
-      <Item title={data[1].title} amount={data[1].amount}/>
-      <Item title={data[2].title} amount={data[2].amount}/>
+      {data.map((el, index) => {
+        {/* return <Item key={index} title={el.title} amount={el.amount}/> */}
+        return <Item key={index} {...el}/> // spread operator
+      })}
     </ul>
   );
 };
